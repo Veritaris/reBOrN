a3e1ff20fc051574145cbdfaa987e9dd868a945e
Georgii Imeshkenov <georgiiimeshkenov@gmail.com>
2026-04-21 00:36:41 +0300

    updating icon file for win;

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     if std::env::var_os("CARGO_CFG_WINDOWS").is_some() {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("resources/icon.icns");
+        res.set_icon("resources/icon.ico");
         res.compile().unwrap();
     }
 }
